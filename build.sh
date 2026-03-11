f2fce58e9b19efcde3d67ca27ffee9116225dc9b
Courville Software <software@courville.org>
2026-03-11 21:55:18 +0100

    create prebuilt dir

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 source ../../AVP/android-setup-light.sh
 
 LOCAL_PATH=$($READLINK -f .)
+mkdir -p ../prebuilt/opus
 PREBUILT_DIR=$($READLINK -f ../prebuilt/opus)
 
 # skip git clone if all prebuilt libs already exist
